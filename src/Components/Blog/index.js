94d22956e850245303a3404ba6f1c42f7a875246
Rameshzende <jhande00@gmail.com>
2023-12-10 11:22:26 +0530

    added the blog details

--- src/Components/Blog/index.js
+++ src/Components/Blog/index.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import './style.css'
+import { blogData } from '../../Utils/data'
 
 const Blog = () => {
   return (
-    <div>Blog</div>
+    <div className='container blog'>
+      {
+        blogData.map((blogItems) => {
+          return(
+            <div className='blog-container'>
+              <div className='left'>
+                   <i class="fa fa-arrow-right" aria-hidden="true"></i>
+                   <div>{blogItems.title}</div>
+              </div>
+              <div> Nov 2, 2022 </div>
+            </div>
+          )
+        })
+      }
+      {/* <i class="fa fa-arrow-right" aria-hidden="true"></i>
+      <div>Basic markdown style guide</div>
+      <div> Nov 2, 2022 </div> */}
+    </div>
   )
 }
 
---
+++ src/Utils/data.js
@@ -0,0 +1,41 @@
+export const blogData = [
+    {
+        title: 'Basic markdown style guide',
+        data: '',
+        isDoc: false
+    },
+    {
+        title: 'Extended markdown style guide',
+        data: '',
+        isDoc: false
+    },
+    {
+        title: 'Prism.js syntax highlighting for code blocks',
+        data: '',
+        isDoc: false
+    },
+    {
+        title: 'You can also link to an external blog post ',
+        data: '',
+        isDoc: true
+    },
+    {
+        title: 'Basic markdown style guide',
+        data: '',
+        isDoc: false
+    }
+]
+
+export const ProjectData = [
+    {
+        title: 'A design system boilerplate with multiple themes',
+        data: '',
+        isDoc: true
+    },
+    {
+        title: 'A minimal landing page for developers',
+        data: '',
+        isDoc: true
+    },
+  
+]
